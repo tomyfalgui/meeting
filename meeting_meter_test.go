cdd201af0f87c53aafc750d69a9d99d6c3cc8b0b
Tomy Falgui <tomyfalgui@gmail.com>
2023-03-08 19:03:32 +0800

    add basic test for rate calculation

--- meeting_meter_test.go
+++ meeting_meter_test.go
@@ -1,1 +1,25 @@
 package meeting_meter_test
+
+import (
+	"meeting_meter"
+	"testing"
+)
+
+func TestCalculateMinuteCost(t *testing.T) {
+	t.Parallel()
+
+	// hourly rate
+	participants := map[string]int{
+		"John":  60,
+		"Alice": 60,
+		"Dave":  60,
+		"Drew":  60,
+	}
+
+	want := 4
+	got := meeting_meter.CalculateMinuteCost(participants)
+
+	if want != got {
+		t.Errorf("want %v != got %v", want, got)
+	}
+}
