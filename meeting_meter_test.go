c010cef0a8835ea9efc2c0dc307b46990187ed04
Tomy Falgui <tomyfalgui@gmail.com>
2023-03-09 23:07:51 +0800

    new test for calculate minute cost

--- meeting_meter_test.go
+++ meeting_meter_test.go
@@ -40,3 +40,26 @@ func TestAddParticipant(t *testing.T) {
 		t.Errorf("Participants() mismatch (-want +got):\n%s", diff)
 	}
 }
+
+func TestCalculateMinuteCost(t *testing.T) {
+	t.Parallel()
+
+	meter := meeting_meter.New()
+	meter.AddParticipant(meeting_meter.Participant{
+		Name:       "John",
+		HourlyRate: 60,
+		JoinTime:   0,
+	})
+	meter.AddParticipant(meeting_meter.Participant{
+		Name:       "Alice",
+		HourlyRate: 60,
+		JoinTime:   0,
+	})
+
+	want := 2
+	got := meter.CalculateMinuteCost()
+
+	if want != got {
+		t.Errorf("want %v != got %v", want, got)
+	}
+}
