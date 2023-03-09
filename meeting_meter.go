a2b088735166fdd13dca1efca5262b2026f702b6
Tomy Falgui <tomyfalgui@gmail.com>
2023-03-09 23:06:04 +0800

    new interface for adding participants

--- meeting_meter.go
+++ meeting_meter.go
@@ -1,22 +1,34 @@
 package meeting_meter
 
+type Participant struct {
+	Name       string
+	HourlyRate int
+	JoinTime   int // exact second participant joined
+}
 type meeting struct {
 	// In cents
 	accruedCost  int
-	participants map[string]int
+	participants []Participant
 }
 
-func New(participants map[string]int) meeting {
+func New() meeting {
 	return meeting{
-		accruedCost:  0,
-		participants: participants,
+		accruedCost: 0,
 	}
+}
+
+func (m *meeting) AddParticipant(p Participant) {
+	m.participants = append(m.participants, p)
+}
+
+func (m meeting) Participants() []Participant {
+	return m.participants
 }
 
 func (m meeting) CalculateMinuteCost() int {
 	totalMinuteCost := 0
-	for _, cost := range m.participants {
-		minuteCost := cost / 60
+	for _, p := range m.participants {
+		minuteCost := p.HourlyRate / 60
 		totalMinuteCost += minuteCost
 	}
 
--- meeting_meter_test.go
+++ meeting_meter_test.go
@@ -3,25 +3,40 @@ package meeting_meter_test
 import (
 	"meeting_meter"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestCalculateMinuteCost(t *testing.T) {
+func TestAddParticipant(t *testing.T) {
 	t.Parallel()
 
-	// hourly rate
-	participants := map[string]int{
-		"John":  60,
-		"Alice": 60,
-		"Dave":  60,
-		"Drew":  60,
+	meter := meeting_meter.New()
+	meter.AddParticipant(meeting_meter.Participant{
+		Name:       "John",
+		HourlyRate: 60,
+		JoinTime:   0,
+	})
+	meter.AddParticipant(meeting_meter.Participant{
+		Name:       "Alice",
+		HourlyRate: 60,
+		JoinTime:   0,
+	})
+
+	want := []meeting_meter.Participant{
+		{
+			Name:       "John",
+			HourlyRate: 60,
+			JoinTime:   0,
+		},
+		{
+			Name:       "Alice",
+			HourlyRate: 60,
+			JoinTime:   0,
+		},
 	}
+	got := meter.Participants()
 
-	meter := meeting_meter.New(participants)
-
-	want := 4
-	got := meter.CalculateMinuteCost()
-
-	if want != got {
-		t.Errorf("want %v != got %v", want, got)
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Participants() mismatch (-want +got):\n%s", diff)
 	}
 }
