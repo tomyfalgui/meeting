1e4dc7644c07b0a805757ed81e201be2f8caba75
Tomy Falgui <tomyfalgui@gmail.com>
2023-03-29 19:50:24 +0800

    add test for printing

--- meeting.go
+++ meeting.go
@@ -2,19 +2,34 @@ package meeting
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
 	"time"
 )
 
 type meeting struct {
 	Participants []int
 	ElapsedTime  time.Duration
+	Output       io.Writer
 }
 
-func NewMeter(participants []int) (meeting, error) {
+type option func(*meeting) error
+
+func NewMeter(participants []int, opts ...option) (meeting, error) {
 	if len(participants) == 0 {
 		return meeting{}, errors.New("Participant list is empty")
 	}
-	return meeting{Participants: participants}, nil
+	m := meeting{Output: os.Stdout, Participants: participants}
+
+	for _, opt := range opts {
+		err := opt(&m)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return m, nil
 }
 
 func (m meeting) CurrentCost() int {
@@ -24,4 +39,27 @@ func (m meeting) CurrentCost() int {
 		totalCost += (c / int(secondCost))
 	}
 	return totalCost
+}
+
+func (m *meeting) StartMeter() *time.Ticker {
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for range ticker.C {
+			m.ElapsedTime++
+			m.Print()
+		}
+	}()
+	return ticker
+}
+
+func (m *meeting) Print() {
+	fmt.Fprintf(m.Output, "Elapsed Seconds: %d\n", m.ElapsedTime)
+	fmt.Fprintf(m.Output, "Total Cost: $%.2f\n\n", float64(m.CurrentCost())/100)
+}
+
+func WithOutput(output io.Writer) option {
+	return func(m *meeting) error {
+		m.Output = output
+		return nil
+	}
 }
--- meeting_test.go
+++ meeting_test.go
@@ -1,7 +1,9 @@
 package meeting_test
 
 import (
+	"bytes"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	meeting "github.com/tomyfalgui/meeting_meter"
@@ -38,5 +40,55 @@ func TestCostCalculationBasedOnElapsedTime(t *testing.T) {
 
 	if want != got {
 		t.Errorf("want %v, got %v", want, got)
+	}
+}
+
+func TestStartMeterIncrementsElapsedTime(t *testing.T) {
+	t.Parallel()
+
+	participants := []int{10000, 10000}
+	fakeTerminal := &bytes.Buffer{}
+	m, _ := meeting.NewMeter(participants, meeting.WithOutput(fakeTerminal))
+	ticker := m.StartMeter()
+
+	time.Sleep(3500 * time.Millisecond)
+	ticker.Stop()
+
+	want := 3
+	got := m.ElapsedTime
+
+	if want != int(got) {
+		t.Errorf("want %v, got %v", want, got)
+	}
+}
+
+func TestWithOutputOption(t *testing.T) {
+	t.Parallel()
+
+	fakeTerminal := &bytes.Buffer{}
+	participants := []int{10000, 10000}
+
+	_, err := meeting.NewMeter(participants, meeting.WithOutput(fakeTerminal))
+	if err != nil {
+		t.Errorf("received err WithOutput")
+	}
+}
+
+func TestPrintMeterWhenStarted(t *testing.T) {
+	t.Parallel()
+
+	fakeTerminal := &bytes.Buffer{}
+	participants := []int{10000, 10000}
+
+	m, _ := meeting.NewMeter(participants, meeting.WithOutput(fakeTerminal))
+	ticker := m.StartMeter()
+	time.Sleep(3500 * time.Millisecond)
+	ticker.Stop()
+
+	want := "Elapsed Seconds: 1\nTotal Cost: $0.04\n\nElapsed Seconds: 2\nTotal Cost: $0.10\n\nElapsed Seconds: 3\nTotal Cost: $0.16\n\n"
+	got := fakeTerminal.String()
+
+	if want != got {
+		t.Errorf("%v", cmp.Diff(want, got))
 	}
 }
