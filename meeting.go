dd158fab97ff4f62ffa465037447b23d68f33e9a
Tomy Falgui <tomyfalgui@gmail.com>
2023-03-29 18:30:55 +0800

    add correct implementation for addParticipant behavior

--- meeting.go
+++ meeting.go
@@ -14,5 +14,5 @@ func NewMeter(participants []int) (meeting, error) {
 	if len(participants) == 0 {
 		return meeting{}, errors.New("Participant list is empty")
 	}
-	return meeting{}, nil
+	return meeting{Participants: participants}, nil
 }
