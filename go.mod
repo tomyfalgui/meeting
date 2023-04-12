e2bfa1cb6a4ac24cc80625b4926e319f0afa64a9
Tomy Falgui <tomyfalgui@gmail.com>
2023-04-12 22:52:35 +0800

    modify based on session

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tomyfalgui/meeting_meter
 
 go 1.19
 
-require github.com/google/go-cmp v0.5.9 // indirect
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,66 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/tomyfalgui/meeting_meter/meeting"
-)
+import "github.com/tomyfalgui/meeting_meter/meeting"
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println(helpText())
-		return
-	}
-
-	totalCost := os.Args[1]
-	conv, err := strconv.Atoi(totalCost)
-	if err != nil {
-		fmt.Println("Please provid a valid number")
-		fmt.Println(helpText())
-		return
-	}
-
-	participants := []int{conv}
-	meter, _ := meeting.NewMeter(participants)
-
-	for range time.NewTicker(time.Second).C {
-		elapsedTime := meter.ElapsedTime()
-		totalCost := meter.TotalCost()
-
-		fmt.Printf(timeFormat(elapsedTime))
-		fmt.Printf("Total Cost: $%.2f\n\n", float64(totalCost)/100)
-	}
-}
-
-func helpText() string {
-	var helpText strings.Builder
-	helpText.WriteString("meeting_meter - commandline meeting cost tracker\n\n")
-	helpText.WriteString("usage: meeting_meter <hourly_cost_in_cents>\n\n")
-	helpText.WriteString(
-		"meeting_meter helps you track the total money wasted in a meeting\n\n",
-	)
-	helpText.WriteString("Example:\n")
-	helpText.WriteString("\tmeeting_meter 30000")
-	return helpText.String()
-}
-
-func timeFormat(seconds int) string {
-	var sb strings.Builder
-	hours := seconds / 3600
-
-	if hours >= 1 {
-		fmt.Fprintf(&sb, "Hours: %d ", hours)
-	}
-	seconds = seconds - (hours * 3600)
-	minutes := seconds / 60
-	if minutes >= 1 {
-		fmt.Fprintf(&sb, "Minutes: %d ", minutes)
-	}
-	seconds = seconds - (minutes * 60)
-	fmt.Fprintf(&sb, "Seconds: %d \n", seconds)
-	return sb.String()
+	meeting.Main()
 }
